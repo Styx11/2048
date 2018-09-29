de5ee9fbd319a6d617ad83d9039d161f52949059
styx11 <1654630248@qq.com>
2018-09-29 16:24:22 +0800

    grid done

--- js/grid.js
+++ js/grid.js
@@ -7,13 +7,21 @@ function Grid (size) {
 Grid.prototype.initGrid = function () {
   var row = this.size;
   var cells = [];
-  for (let x=0; x<row; x++) {
-    cells[x] = [];
-    for (let y=0; y<row; y++) {
-      cells[x].push(null);
+  for (let y=0; y<row; y++) {
+    cells[y] = [];
+    for (let x=0; x<row; x++) {
+      cells[y].push(null);
     }
   }
   this.cells = cells;
+}
+// 随机获取单元格
+Grid.prototype.randomCells = function () {
+  var availableCells = this.availableCells();
+  var availLength = availableCells.length;
+  var index = Math.floor(Math.random() * availLength);
+  var randomCell = availableCells[index];
+  return randomCell;
 }
 // 获取可用单元格
 Grid.prototype.availableCells = function () {
@@ -32,13 +40,19 @@ Grid.prototype.availableCells = function () {
     return avail;
   }
 }
-// 随机获取单元格
-Grid.prototype.randomCells = function () {
-  var availableCells = this.availableCells();
-  var availLength = availableCells.length;
-  var index = Math.floor(Math.random() * availLength);
-  var randomCell = availableCells[index];
-  return randomCell;
+Grid.prototype.updataCell = function (type, position) {
+  var posX = position.posX;
+  var posY = position.posY;
+  if (type === 'remove') {
+    this.cells[posY][posX] = null;
+  } else if (type === 'fill') {
+    this.cells[posY][posX] = {
+      posX: posX,
+      posY: posY
+    }
+  }
 }
 var grid = new Grid(4);
 grid.initGrid();
+console.log(grid.cells);
+console.log(grid.availableCells());
---
+++ js/tile.js
@@ -0,0 +1,15 @@
+function Tile (position, value) {
+  this.position = position,
+  this.value = value
+}
+Tile.prototype.createTile = function () {
+  var posX = this.position.posX;
+  var posY = this.position.posY;
+  var tile = document.createElement('div');
+  var container = document.getElementsByClassName('container')[0];
+  tile.innerHTML = this.value;
+  tile.className = 'object';
+  container.appendChild(tile);
+}
+var tile = new Tile({}, 2);
+tile.createTile();
