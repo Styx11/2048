f3b1815d6ef1063aea53102c251247d7cd128dd2
styx11 <1654630248@qq.com>
2018-09-30 09:43:01 +0800

    create tile

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 var grid = new Grid(4);
 grid.initGrid();
+var randomCell = grid.randomCells();
 console.log(grid.cells);
 console.log(grid.availableCells());
 console.log(grid.availableCellInline({posX: 1, posY: 0}, 3, 'col'));
+
+var tile = new Tile(randomCell, 2);
+tile.createTile();
+grid.updataCell('fill', randomCell);
+console.log(grid.cells);
--- js/tile.js
+++ js/tile.js
@@ -1,13 +1,16 @@
 function Tile (position, value) {
-  this.position = position,
-  this.value = value
+  this.position = position;
+  this.value    = value;
 }
+Tile.prototype.container = document.getElementsByClassName('container')[0];
+// 根据randomCell创建瓦片
 Tile.prototype.createTile = function () {
-  var posX = this.position.posX;
-  var posY = this.position.posY;
-  var tile = document.createElement('div');
-  var container = document.getElementsByClassName('container')[0];
-  tile.innerHTML = this.value;
-  tile.className = 'object';
-  container.appendChild(tile);
+  var newTile   = document.createElement('div');
+
+  newTile.innerHTML = this.value;
+  newTile.classList.add('object');
+  newTile.style.top = (this.position.posY * 120) + 'px';
+  newTile.style.left = (this.position.posX * 120) + 'px';
+
+  this.container.appendChild(newTile);
 }
