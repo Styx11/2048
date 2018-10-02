b2dda6c13e14a7f4209d986a684e5c3a1878c673
styx11 <1654630248@qq.com>
2018-10-02 17:24:25 +0800

    moveCells function debugged

--- js/grid.js
+++ js/grid.js
@@ -50,9 +50,9 @@ Grid.prototype.availableCellInline = function (cell, end, line) {
   var availableCellInline = null;
 
   if (line === 'row') {
-    var x = posX < end ? posX+1 : posX-1;
-    end = posX < end ? end+1 : end-1;
-    while (x !== end) {
+    var x = posX < end ? posX+1 : posX-1;// 判断起终点
+    var newEnd = posX < end ? end+1 : end-1;
+    while (x !== newEnd) {
       if (!this.cells[posY][x] || this.cells[posY][x].value === value) {
         availableCellInline = {
           posX: x,
@@ -62,7 +62,7 @@ Grid.prototype.availableCellInline = function (cell, end, line) {
       } else {
         break;
       }
-      posX < end ? x++ : x--;
+      x < newEnd ? x++ : x--;
     }
   } else if (line === 'col') {
     var y = posY < end ? posY+1 : posY-1;
@@ -77,7 +77,7 @@ Grid.prototype.availableCellInline = function (cell, end, line) {
       } else {
         break;
       }
-      posY < end ? y++ : y--;
+      y < newEnd ? y++ : y--;
     }
   }
   return availableCellInline;
@@ -98,43 +98,45 @@ Grid.prototype.updataCell = function (type, position, value) {
 }
 // 移动栅格
 Grid.prototype.moveCells = function (key) {
-  var that = this;
-  var movingCells = [];
-  var deletingCells = [];
+  var redrawCells = [];
+  // 判断从哪个方向开始移动
+  var start = key===2 || key===3 ? 3 : 0;
+  var end = key===2 || key===3 ? -1 : 4;
 
-  if (key === 0 || key === 2) {
+  if (key === 0 || key === 2) {// 左右
     var direct = !key ? 0 : 3;
-    for (let x=0; x<this.size; x++) {
+    while (start !== end) {
       for (let y=0; y<this.size; y++) {
-        if (this.cells[y][x] !== null) {
-          var avail = this.availableCellInline({posX: x, posY: y, value: this.cells[y][x].value}, direct, 'row');
+        if (this.cells[y][start] !== null) {// 获取单方向上单元格可移动的位置
+          var avail = this.availableCellInline({posX: start, posY: y, value: this.cells[y][start].value}, direct, 'row');
           if (avail !== null) {
-            movingCells.push(avail);
-            deletingCells.push(this.cells[y][x]);
-            this.updataCell('remove', {posX: x, posY: y});
+            redrawCells.push(avail);
+            this.updataCell('remove', {posX: start, posY: y});
             this.updataCell('fill', {posX: avail.posX, posY: avail.posY}, avail.value);
+          } else {
+            redrawCells.push(this.cells[y][start]);// 无可移动单元格也需重绘
           }
         }
       }
+      start < end ? start++ : start--;
     }
-  } else if (key === 1 || key === 3) {
+  } else if (key === 1 || key === 3) {// 上下
     var direct = key === 1 ? 0 : 3;
-    for (let y=0; y<this.size; y++) {
+    while (start !== end) {
       for (let x=0; x<this.size; x++) {
-        if (this.cells[y][x] !== null) {
-          var avail = this.availableCellInline({posX: x, posY: y, value: this.cells[y][x].value}, direct, 'col');
+        if (this.cells[start][x] !== null) {
+          var avail = this.availableCellInline({posX: x, posY: start, value: this.cells[start][x].value}, direct, 'col');
           if (avail !== null) {
-            movingCells.push(avail);
-            deletingCells.push(this.cells[y][x]);
-            this.updataCell('remove', {posX: x, posY: y});
+            redrawCells.push(avail);
+            this.updataCell('remove', {posX: x, posY: start});
             this.updataCell('fill', {posX: avail.posX, posY: avail.posY}, avail.value);
+          } else {
+            redrawCells.push(this.cells[start][x]);
           }
         }
       }
+      start < end ? start++ : start--;
     }
   }
-  return {
-    movingCells: movingCells,
-    deletingCells: deletingCells
-  }
+  return redrawCells;
 }
--- js/index.js
+++ js/index.js
@@ -1,39 +1,27 @@
 var grid = new Grid(4);
-var keyManager = new KeyboardManager();
+var tile = new Tile();
+var KeyboardManager = new KeyboardManager();
 
-console.log(grid.cells);
 // console.log(grid.availableCellInline({posX: 1, posY: 3, value: 2}, 0, 'col'));
 
-keyManager.on("move", function (key) {
+KeyboardManager.on("move", function (key) {
   if (key === 0 || key === 2 || key === 1 || key === 3) {
     var randomCell = grid.randomCell();
-    var tile = new Tile();
     if (randomCell !== undefined) {
       if (grid.availableCells().length === 16) {
         tile.createTile(randomCell, randomCell.value);
         grid.updataCell('fill', randomCell, randomCell.value);
       } else {
-        var moveCells = grid.moveCells(key);
-        var movingCells = moveCells.movingCells;
-        var deletingCells = moveCells.deletingCells;
-        // for (let i=0; i<deletingCells.length; i++) {
-        //   tile.delTile(deletingCells[i]);
-        // }
+        var redrawCells = grid.moveCells(key);
         tile.delTile();
-        // for (let i=0; i<movingCells.length; i++) {
-        //   tile.createTile(movingCells[i], movingCells[i].value);
-        // }
-        for (let y=0; y<grid.size; y++) {
-          for (let x=0; x<grid.size; x++) {
-            if (grid.cells[y][x] !== null) {
-              tile.createTile({posX: x, posY: y}, grid.cells[y][x].value);
-            }
-          }
+        for (let i=0; i<redrawCells.length; i++) {
+          tile.createTile(redrawCells[i], redrawCells[i].value);
         }
+        randomCell = grid.randomCell();
         tile.createTile(randomCell, randomCell.value);
         grid.updataCell('fill', randomCell, randomCell.value);
+        console.log(grid.cells);
       }
-      console.log(grid.cells);
     }
   }
 })
