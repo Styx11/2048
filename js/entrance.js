9358eb9dc55af55e4280c464f111c1fccf5f6fe6
styx11 <1654630248@qq.com>
2018-10-03 17:02:47 +0800

    check window.localStorage compatibility

--- js/entrance.js
+++ js/entrance.js
@@ -2,59 +2,59 @@ var grid = new Grid(4);
 var tile = new Tile();
 var KeyboardManager = new KeyboardManager();
 var localStorageManager = new localStorageManager();
-var bestScore = localStorageManager.getBestScore();
-var previousState = localStorageManager.getGameState();
-var scoreTile = parseInt(tile.scoreTile.innerHTML) + grid.score;
-var start = function () {
+var bestScore     = localStorageManager.getBestScore();
+var previousState = localStorageManager.getGameState('gameState');
+var previousScore = localStorageManager.getGameState('gameScore');
+
+var start = function () {// 基本开始
   var randomCell = grid.randomCell();
   tile.createTile(randomCell, randomCell.value);
   grid.updataCell('fill', randomCell, randomCell.value);
-  tile.bestTile.innerHTML = bestScore;
-  tile.scoreTile.innerHTML = scoreTile;
+  tile.bestTile.innerHTML  = bestScore;// DOM对象只需引用一次
+  tile.scoreTile.innerHTML = previousScore;
 }
 
-if (previousState) {
+if (previousState) {// 是否应用先前状态
   grid.cells = previousState;
   var drawCells = grid.availableCells().unavail;
   for (let i=0; i<drawCells.length; i++) {
     tile.createTile(drawCells[i], drawCells[i].value);
   }
-  console.log(grid.cells);
-  console.log(drawCells);
-  tile.bestTile.innerHTML = bestScore;
-  tile.scoreTile.innerHTML = scoreTile;
+  tile.bestTile.innerHTML  = bestScore;
+  tile.scoreTile.innerHTML = previousScore;
 } else {
   start();
 }
+// 游戏重开
 tile.newGame.onclick = function () {
+  localStorageManager.clearGameState();// 清空本地状态
+  previousScore = localStorageManager.getGameState('gameScore');// 重新获得分数
+  tile.scoreTile.innerHTML = previousScore;
   tile.delTile();
   grid.initGrid();
   start();
+  localStorageManager.setGameState(grid.cells);
 }
 KeyboardManager.on("move", function (key) {
   if (key === 0 || key === 2 || key === 1 || key === 3) {
     var randomCell = grid.randomCell();
     if (randomCell !== undefined) {
-      if (grid.availableCells().length === 16) {
-        tile.createTile(randomCell, randomCell.value);
-        grid.updataCell('fill', randomCell, randomCell.value);
-      } else {
-        var redrawCells = grid.moveCells(key);// 移动
-        if (grid.score) {// 记录分数
-          scoreTile += grid.score;
-          if (scoreTile > bestScore) {
-            bestScore = scoreTile;
-            localStorageManager.setBestScore(scoreTile);
-          }
+      var redrawCells = grid.moveCells(key);// 移动
+      if (grid.score) {// 记录分数
+        previousScore += grid.score;
+        if (previousScore > bestScore) {// 设置最高分
+          bestScore = previousScore;
+          localStorageManager.setBestScore(previousScore);
         }
-        tile.delTile();// 删除单元格
-        for (let i=0; i<redrawCells.length; i++) {// 创建单元格
-          tile.createTile(redrawCells[i], redrawCells[i].value);
-        }
-        start();
-        grid.score = 0;// 重置分数
-        localStorageManager.setGameState(grid.cells);
       }
+      tile.delTile();// 删除单元格
+      for (let i=0; i<redrawCells.length; i++) {// 创建单元格
+        tile.createTile(redrawCells[i], redrawCells[i].value);
+      }
+      start();
+      localStorageManager.setGameState('gameState', grid.cells);// 记录状态
+      localStorageManager.setGameState('gameScore', previousScore);
+      grid.score = 0;// 重置分数
     }
   }
 })
--- js/localStorage_manager.js
+++ js/localStorage_manager.js
@@ -1,18 +1,49 @@
+if (!window.localStorage) {
+  window.localStorage = {
+    getItem: function (sKey) {
+      if (!sKey || !this.hasOwnProperty(sKey)) { return null; }
+      return unescape(document.cookie.replace(new RegExp("(?:^|.*;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*"), "$1"));
+    },
+    key: function (nKeyId) {
+      return unescape(document.cookie.replace(/\s*\=(?:.(?!;))*$/, "").split(/\s*\=(?:[^;](?!;))*[^;]?;\s*/)[nKeyId]);
+    },
+    setItem: function (sKey, sValue) {
+      if(!sKey) { return; }
+      document.cookie = escape(sKey) + "=" + escape(sValue) + "; expires=Tue, 19 Jan 2038 03:14:07 GMT; path=/";
+      this.length = document.cookie.match(/\=/g).length;
+    },
+    length: 0,
+    removeItem: function (sKey) {
+      if (!sKey || !this.hasOwnProperty(sKey)) { return; }
+      document.cookie = escape(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+      this.length--;
+    },
+    hasOwnProperty: function (sKey) {
+      return (new RegExp("(?:^|;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+    }
+  };
+  window.localStorage.length = (document.cookie.match(/\=/g) || window.localStorage).length;
+}
+
 function localStorageManager () {
-  window.fakeStorage = {};
   this.gameState = "gameState";
+  this.gameScore = 'gameScore';
   this.bestScore = "bestScore";
-  this.storageManager = window.localStorage ? window.localStorage : window.fakeStorage;
+  this.storageManager = window.localStorage;
 
   this.setBestScore(0);
 }
-localStorageManager.prototype.getGameState = function () {
-  var gameState = JSON.parse(this.storageManager.getItem(this.gameState));
-  return gameState;
+localStorageManager.prototype.getGameState = function (item) {
+  var gameState = JSON.parse(this.storageManager.getItem(this[item]));
+  return gameState ? gameState : 0;
 }
-localStorageManager.prototype.setGameState = function (value) {
+localStorageManager.prototype.setGameState = function (item, value) {
   var gameState = JSON.stringify(value);
-  this.storageManager.setItem(this.gameState, gameState);
+  this.storageManager.setItem(this[item], gameState);
+}
+localStorageManager.prototype.clearGameState = function () {
+  this.storageManager.removeItem(this.gameState);
+  this.storageManager.removeItem(this.gameScore);
 }
 localStorageManager.prototype.getBestScore = function () {
   var bestScore = this.storageManager.getItem(this.bestScore);
